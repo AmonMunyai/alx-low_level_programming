807e55186a66931408ebe0ee56b411ac0cde10d8
AmonMunyai <Amonmunyai11@gmail.com>
2022-10-18 14:35:03 +0200

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int i = 0;
+
+	while (i <= 10)
+	{
+		alpha = 'a';
+
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		i++;
+		_putchar('\n');
+	}
+	return (0);
+}
