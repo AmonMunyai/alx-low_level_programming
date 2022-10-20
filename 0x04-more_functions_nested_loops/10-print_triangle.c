8e104cfc8fe3b770acff6f6a080ffbd98d2dd34b
AmonMunyai <Amonmunyai11@gmail.com>
2022-10-20 20:09:11 +0200

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: size of the triangle
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int rows, cols, c;
+
+	for (rows = 0; rows < size; rows++) /* rows */
+	{
+		for (cols = size - 1; cols > rows; cols--) /* print spaces */
+			_putchar('.');
+		for (c = 0; c < rows + 1; c++) /* print characters */
+			_putchar('#');
+		putchar('\n');
+	}
+}
