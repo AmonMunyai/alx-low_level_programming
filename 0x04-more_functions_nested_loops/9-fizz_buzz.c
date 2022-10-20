98c8c25a362f287983c21ce8dd25ac836c1764fb
AmonMunyai <Amonmunyai11@gmail.com>
2022-10-20 18:59:11 +0200

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,22 +14,27 @@ int main(void)
 	{
 		if (n % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (n % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if (n % 15 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else
 		{
-			printf("%d ", n);
+			printf("%d", n);
+		}
+
+		if (n < 100)
+		{
+			_putchar(' ');
 		}
 	}
-	putchar('\n');
+	_putchar('\n');
 
 	return (0);
 }
