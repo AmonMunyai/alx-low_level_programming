bc74e344654995062c18a4e93fde20993d19b67c
AmonMunyai <Amonmunyai11@gmail.com>
2022-10-20 21:28:28 +0200

    Numbers have life; they're not just symbols on paper

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -19,7 +19,7 @@ void print_number(int n)
 	else if (n > 99 && n <= 999)
 	{
 		_putchar((n / 100) + '0');
-		_putchar(((n % 10) / 10)+ '0');
+		_putchar(((n % 10) / 10) + '0');
 		_putchar((n % 10) + '0');
 	}
 	else if (n > 999 && n <= 9999)
