b6d541f158f1bc7809fdbfa92d400ad1f91be8a6
AmonMunyai <Amonmunyai11@gmail.com>
2022-10-27 07:50:49 +0200

    Thu Oct 27 07:50:17 2022

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,22 +3,27 @@
 /**
  * cap_string - capitalizes all words of a string
  * @src: string to capitalize
+ * Return: pointer to string
  */
 
 char *cap_string(char *src)
 {
-    char sep[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
-    int i, j;
+	char sep[] = {' ', '\t', '\n', ',', ';',
+		'.', '!', '?', '"', '(', ')', '{', '}'};
+	int i, j;
 
-    for (i = 0; src[i] != '\0'; i++)
-    {
-        if (src[i] >= 'a' && src[i] <= 'z') /* lowercase letters */
-        {
-            for (j = 0; j < 13; j++) /* capitalize if preceeded by sep character */
-                if (src[i - 1] == sep[j])
-                    src[i] -= 32;
-        }
-    }
+	i = 0;
 
-    return (src);
+	while (src[i] != '\0')
+	{
+		while (!(src[i] >= 'a' && src[i] <= 'z'))
+			i++;
+
+		for (j = 0; src[i - 1] == sep[j]; j++)
+			src[i] -= 32;
+
+		i++;
+	}
+
+	return (src);
 }
