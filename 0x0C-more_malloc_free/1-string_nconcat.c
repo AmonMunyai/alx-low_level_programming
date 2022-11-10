c2344cba44db8c66039e04ae50c8511dbe901b9c
AmonMunyai <Amonmunyai11@gmail.com>
2022-11-10 18:55:21 +0200

    allocated space for null terminator

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,7 +24,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i] != '\0'; i++) /* add len of s1 */
 		len++;
 
-	str = malloc(sizeof(*str) * len); /* allocate memory */
+	str = malloc(sizeof(*str) * (len + 1)); /* allocate memory */
 
 	if (str == NULL)
 		return (NULL);
