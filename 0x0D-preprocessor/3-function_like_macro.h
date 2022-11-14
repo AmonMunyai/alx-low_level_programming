5d30299f303c928ea195ec9b5dcd386b1a401818
AmonMunyai <Amonmunyai11@gmail.com>
2022-11-14 17:49:15 +0200

    added description

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,10 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
+/**
+ * Desc: computes the absolute value of x
+ */
+
 #define ABS(x) (((x) < 0) ? (-x) : (x))
 
 #endif /* FUNCTION_LIKE_MACRO_H */
