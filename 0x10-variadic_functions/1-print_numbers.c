fcfd985c52bd1e73bc41e1130e32b42e74820ffa
AmonMunyai <Amonmunyai11@gmail.com>
2022-11-17 18:20:14 +0200

    initial commit

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,19 @@
+#include "variadic_functions.h"
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+
+		if (separator != NULL && i + 1 < n)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
+}
