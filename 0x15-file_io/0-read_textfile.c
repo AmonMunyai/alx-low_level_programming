1726a9a9475d46148c2bc7ea0830f02a2b2ab85d
AmonMunyai <Amonmunyai11@gmail.com>
2022-12-06 01:21:55 +0200

    betty style

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * read_textfile -  reads a text file and prints it to
+ * the POSIX standard output.
+ * @filename: the name of file to open and read.
+ * @letters: the number of letters it should read and print.
+ * Return: the actual number of letters it could read and print.
+*/
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, fp, count;
