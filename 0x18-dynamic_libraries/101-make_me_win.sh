988deb76c78022fdc965671ae597eb2e5e04587b
AmonMunyai <60216128+AmonMunyai@users.noreply.github.com>
2023-12-12 00:11:50 +0200

    fix: incorrect link to libgiga.so

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/AmonMunyai/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+wget -P /tmp https://github.com/AmonMunyai/alx-low_level_programming/blob/a598ea9dcbde3bccf0056abe97360e5b8ab16326/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
